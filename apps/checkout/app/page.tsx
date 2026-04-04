5d95849b47e29e0b51ad89f5844326d0d88c7fe5
mar-alv <mdsantos.alvarez@gmail.com>
2026-04-04 15:28:09 -0300

    feat: adding checkout project

---
+++ apps/checkout/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-500">
+      checkout
+      <Link href="http://localhost:3001">voltar</Link>
+    </div>
+  );
+}
