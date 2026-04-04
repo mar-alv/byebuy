2063fc0f4bdf18b14c769b4fc8337829fc426dbc
mar-alv <mdsantos.alvarez@gmail.com>
2026-04-04 15:26:47 -0300

    feat: adding auth project

---
+++ apps/auth/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="bg-red-500">
+      login
+      <Link href="http://localhost:3001">ir pro catalogo</Link>
+    </div>
+  );
+}
