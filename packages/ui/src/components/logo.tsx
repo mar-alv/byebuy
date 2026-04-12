9a71d2d1bc50598ee77299e528038c8e551bad0c
mar-alv <mdsantos.alvarez@gmail.com>
2026-04-12 17:00:55 -0300

    refactor: using imgs for the logo

--- packages/ui/src/components/logo.tsx
+++ packages/ui/src/components/logo.tsx
@@ -1,4 +1,19 @@
-// TODO: replace with an actual img
 export function Logo() {
-  return <div className="w-[100px] h-[30px] bg-red-500"></div>;
+  return (
+    <img
+      src="/images/logo.png"
+      alt="Logotipo com uma sacola de compras estilizada em tons de laranja, com rosto sorridente e pernas em movimento, acompanhado do texto 'BYEBUY' em fonte arredondada abaixo."
+      className="size-18"
+    />
+  );
 }
+
+Logo.NoTexts = function LogoNoTexts() {
+  return (
+    <img
+      src="/images/logo-no-texts.png"
+      alt="Logotipo com uma sacola de compras estilizada em tons de laranja, com rosto sorridente e pernas em movimento."
+      className="size-18"
+    />
+  );
+};
