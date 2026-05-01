1432a09a4d6e982a95b1692901f55cf5a4305074
mar-alv <mdsantos.alvarez@gmail.com>
2026-05-01 11:07:59 -0300

    feat: adding urls to the apis

--- packages/configs/index.ts
+++ packages/configs/index.ts
@@ -3,4 +3,7 @@ export const urls = {
   catalog: "http://localhost:3001",
   checkout: "http://localhost:3002",
   seller: "http://localhost:3003",
+  catalogApi: "http://localhost:3004",
+  checkoutApi: "http://localhost:3005",
+  sellerApi: "http://localhost:3006",
 };
