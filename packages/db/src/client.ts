6159ac46d81b4d140c1f92b5e005b7f5e29d4d06
mar-alv <mdsantos.alvarez@gmail.com>
2026-05-02 11:41:01 -0300

    fix: correcting db to work with better sqlite 3

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import path from "path";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const dbPath = path.resolve(__dirname, "../dev.db");
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
+
+const adapter = new PrismaBetterSqlite3({ url: `file:${dbPath}` });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
