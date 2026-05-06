922118a970af5ed33ec815c0dc2f3582ee95866c
mar-alv <mdsantos.alvarez@gmail.com>
2026-05-06 20:13:20 -0300

    feat: adding product count in the cart

--- packages/ui/src/components/header/components/cart-button.tsx
+++ packages/ui/src/components/header/components/cart-button.tsx
@@ -1,17 +1,43 @@
 "use client";
 
+import { useCartStore } from "@repo/cart-store";
 import { urls } from "@repo/configs";
 import { ShoppingCart } from "lucide-react";
-import { Button } from "../../ui/button";
 import Link from "next/link";
+import { Button } from "../../ui/button";
 
 export function CartButton() {
-  // TODO: when signed-in show count in cart, if there is
+  const items = useCartStore((state) => state.items);
+
+  const totalUnits = items.reduce((sum, item) => sum + item.inCart, 0);
 
   return (
-    <Button asChild size="icon" variant="ghost">
+    <Button asChild size="icon" variant="ghost" className="relative">
       <Link href={`${urls.checkout}/cart`}>
-        <ShoppingCart className="h-[1.2rem] w-[1.2rem] scale-100 transition-all" />
+        <ShoppingCart className="h-[1.2rem] w-[1.2rem]" />
+
+        {totalUnits > 0 && (
+          <span
+            className="
+              absolute
+              -top-1
+              -right-1
+              flex
+              h-5
+              min-w-5
+              items-center
+              justify-center
+              rounded-full
+              bg-red-500
+              px-1
+              text-[10px]
+              font-bold
+              text-white
+            "
+          >
+            {totalUnits}
+          </span>
+        )}
       </Link>
     </Button>
   );
